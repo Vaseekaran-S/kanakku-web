0561b443eb403adecf9f602f6e2b4c44b2151b4a
Vaseekaran-S <vaseekaransaminathan@gmail.com>
2024-07-21 09:00:54 +0530

    doc: welcome section text

--- src/components/sections/welcome/index.jsx
+++ src/components/sections/welcome/index.jsx
@@ -6,7 +6,7 @@ function Welcome() {
         <div className='min-h-[90vh] w-full flex-center'>
             <div className='flex flex-col items-center'>
                 <h2 className='text-[30px] md:text-[45px] font-bold text-green-600'>
-                    <GradientText>Welcome to Kannaku!</GradientText>
+                    <GradientText>Welcome to Kanakku!</GradientText>
                 </h2>
                 <p className='font-medium text-lg mt-1'>Transform Your Manual Accounting into a Digital Masterpiece</p>
                 <img src="/images/vectors/welcome.png" alt="Welcome to Kanakku" className='max-h-[250px] mt-3 object-contain' />
