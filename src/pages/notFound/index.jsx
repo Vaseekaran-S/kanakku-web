b7cebfeaeb6f9431ae1eb9342bf21d812db51537
Vaseekaran-S <vaseekaransaminathan@gmail.com>
2024-08-10 15:30:29 +0530

    feat: 404 page

--- src/pages/notFound/index.jsx
+++ src/pages/notFound/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function PageNotFound() {
     return (
-        <div>
-            404
+        <div className='min-h-[80vh] flex-center'>
+            <div className='text-center'>
+                <img src="/images/vectors/404.png" alt="Page Not Found" className='max-h-[350px]'/>
+                <p className='text-lg'>This page is Not Found</p>
+                <Link to="/" className='underline mt-2 text-blue-800 hover:text-blue-500'>Go to Home</Link>
+            </div>
         </div>
     )
 }
