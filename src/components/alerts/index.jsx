80591f7c37ce8faf44b7772daa6930c494d49226
Vaseekaran-S <vaseekaransaminathan@gmail.com>
2024-08-11 22:44:33 +0530

    fix: styling issues in alert

--- src/components/alerts/index.jsx
+++ src/components/alerts/index.jsx
@@ -2,18 +2,15 @@ import React from 'react'
 
 function Alert({ message, type }) {
     const statusColors = {
-        success: "green",
-        error: "red",
-        warning: "orange"
+        success: "border-b-[green]",
+        error: "border-b-[red]",
+        warning: "border-b-[orange]"
     }
     const status = statusColors[type];
 
-    console.log("Status: ", status);
-    
-
     return (
         <div className={`fixed right-[20px] bottom-[20px] flex `}>
-            <div className={`border rounded bg-white flex px-3 py-1 border-b-4 border-b-[${status.trim()}]`}>
+            <div className={`border rounded bg-white flex px-3 py-1 border-b-4 ${status}`}>
                 <p className='font-medium'>{message}</p>
             </div>
         </div>
--- src/components/formik/field.jsx
+++ src/components/formik/field.jsx
@@ -12,7 +12,7 @@ function FormikField({ name, errorMsg, type, ...props }) {
     <div className='mb-3 relative'>
       <Field name={name} {...props} type={isPassword && isPasswordVisible ? "text" : type} className="w-full border border-green-300 px-3 py-2 rounded focus:outline-none focus:border-green-600" />
       {isPassword && <button type='button' onClick={() => setIsPasswordVisible(!isPasswordVisible)} className='absolute top-[13px] right-[10px]'>{isPasswordVisible ? <FaEye /> : <FaEyeSlash />}</button>}
-      <ErrorMessage name={name} component="div" className='text-sm font-medium text-red-600 pl-1' />
+      <ErrorMessage name={name} component="div" className='text-xs font-medium text-red-600 pl-1' />
     </div>
   )
 }
