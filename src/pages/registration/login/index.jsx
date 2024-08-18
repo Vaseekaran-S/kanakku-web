a26ad9495aebd4721d173f720ce8d3d0347005ce
Vaseekaran-S <vaseekaransaminathan@gmail.com>
2024-08-18 16:47:54 +0530

    perf: login page

--- src/pages/registration/login/index.jsx
+++ src/pages/registration/login/index.jsx
@@ -1,30 +1,26 @@
-
-import React, { useState } from 'react'
-import RegistrationForm from 'components/sections/registrationForm'
-
-import inputs from 'data/inputs/login'
+import React from 'react'
 import { useNavigate } from 'react-router-dom';
-import { userLogin } from 'api/registration';
-import Alert from 'components/alerts';
+
 import { useDispatch } from 'react-redux';
 import { setAuthentication } from 'redux-store/user/userSlice';
+
+import { userLogin } from 'api/registration';
+import RegistrationForm from 'components/sections/registrationForm'
+import inputs from 'data/inputs/login'
+import useKanakkuApi from 'hooks/api';
 
 function Login() {
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const [popup, setPopup] = useState({ isVisible: false, message: '', type: '' });
+    const kanakkuApiCall = useKanakkuApi();
 
     const onSubmit = async (data) => {
-        const res = await userLogin(data);
-        setPopup({ isVisible: true, message: res.message, type: res.type });
+        const res = await kanakkuApiCall(userLogin, data)
         if(res?.type === 'success'){
             localStorage.setItem("kanakku-user-token", res?.token)
             dispatch(setAuthentication(true));
             return navigate('/');
         }
-        setTimeout(() => {
-            setPopup(prev => ({ ...prev, isVisible: false }));
-        }, 5000);
     };
 
     const renderingData = {
@@ -39,10 +35,8 @@ function Login() {
         onSubmit
     }
 
-    const { isVisible, message, type } = popup;
     return (
         <div className="min-h-[90vh] w-full flex-center">
-            { isVisible && <Alert message={message} type={type}/> }
             <RegistrationForm type="login" renderingData={renderingData}/>
         </div>
     )
