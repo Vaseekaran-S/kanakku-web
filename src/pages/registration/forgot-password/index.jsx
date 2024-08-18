62a231f7e22fc70165d5af8b508c3f01c64ce5a1
Vaseekaran-S <vaseekaransaminathan@gmail.com>
2024-08-19 00:10:04 +0530

    fear: forgot password page ui

--- src/pages/registration/forgot-password/index.jsx
+++ src/pages/registration/forgot-password/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Form, Formik } from 'formik';
 import FormikField from 'components/formik/field';
@@ -7,14 +7,17 @@ import * as YUP from 'yup'
 import PrimaryBtn from 'components/buttons/primary'
 import PrimaryLink from 'components/links/primary'
 import { forgotPassword } from 'api/registration';
-import { APICALL } from 'utils/api';
-import { useDispatch } from 'react-redux';
+import useKanakkuApi from 'hooks/api';
 
 function ForgotPassword() {
-  const dispatch = useDispatch();
+  const kanakkuApiCall = useKanakkuApi();
+  const [isEmailSend, setIsEmailSend] = useState(false);
 
   const formSubmit = async (values) => {
-    const res = await APICALL(forgotPassword,values?.email, dispatch)
+    const response = await kanakkuApiCall(forgotPassword, values?.email)
+    if (response?.type === "success") {
+      setIsEmailSend(true);
+    }
   }
 
   return (
@@ -23,20 +26,26 @@ function ForgotPassword() {
         <img src="/images/vectors/forgot-password.png" alt="Forgot Password" className='w-full' />
       </div>
       <div className='flex-center lg:w-[50%]'>
-        <div className="rounded p-10 border bg-gray-100">
-          <h3 className='font-bold text-xl text-center mb-3'>Forgot your password?</h3>
-          <p className='max-w-[400px] mb-5'>No problem! Enter your email address below, and we'll send you a link to reset your password.</p>
-          <Formik
-            initialValues={{ email: "" }}
-            validationSchema={YUP.object({ email: YUP.string().required("Email is required!").email('Invalid Email format') })}
-            onSubmit={formSubmit}>
-            <Form>
-              <FormikField placeholder="Enter Email ID" type="email" name="email" />
-              <PrimaryBtn label="Continue" customCss="mt-1 w-full" type="submit" />
-            </Form>
-          </Formik>
-          <p className='text-center mt-5'>Don't have a account? <PrimaryLink to="/signup">SignUp</PrimaryLink> </p>
-        </div>
+        {!isEmailSend ?
+          <div className="rounded p-10 border bg-gray-100">
+            <h3 className='font-bold text-xl text-center mb-3'>Forgot your password?</h3>
+            <p className='max-w-[400px] mb-5'>No problem! Enter your email address below, and we'll send you a link to reset your password.</p>
+            <Formik
+              initialValues={{ email: "" }}
+              validationSchema={YUP.object({ email: YUP.string().required("Email is required!").email('Invalid Email format') })}
+              onSubmit={formSubmit}>
+              <Form>
+                <FormikField placeholder="Enter Email ID" type="email" name="email" />
+                <PrimaryBtn label="Continue" customCss="mt-1 w-full" type="submit" />
+              </Form>
+            </Formik>
+            <p className='text-center mt-5'>Don't have a account? <PrimaryLink to="/signup">SignUp</PrimaryLink> </p>
+          </div> :
+          <div className="rounded p-10 border text-center max-w-[450px]">
+            <img src="/images/gif/email-send.gif" alt="Email Send!" className='max-h-[150px] m-auto' />
+            <h2 className='font-bold text-lg my-3'>Check your Inbox!</h2>
+            <p>We've already send out the reset password link. Please check it and change your password.</p>
+          </div>}
       </div>
     </div>
   )
