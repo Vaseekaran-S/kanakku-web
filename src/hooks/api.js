841f42e41e651b8cbb6c655bc80ed05f923b1fa4
Vaseekaran-S <vaseekaransaminathan@gmail.com>
2024-08-18 16:47:06 +0530

    feat: custom hook for handle api call and popups

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -1,9 +1,18 @@
-import { setLoaderStatus, setPopupAlert } from 'redux-store/popups/popupSlice'
+import { useDispatch } from 'react-redux';
+import { setLoaderStatus, setPopupAlert } from 'redux-store/popups/popupSlice';
 
-export const useKanakkuApi = async (fun, data, dispatch) => {
-    dispatch(setLoaderStatus(true));
-    const response = await fun(data);
-    dispatch(setLoaderStatus(false));
-    dispatch(setPopupAlert({ alertStatus: true, alertMessage: response?.message, alertType: response?.type }))
-    return response;
-}
+const useKanakkuApi = () => {
+    const dispatch = useDispatch();
+
+    const apiCall = async (fun, data) => {
+        dispatch(setLoaderStatus(true));
+        const response = await fun(data);
+        dispatch(setLoaderStatus(false));
+        dispatch(setPopupAlert({ alertStatus: true, alertMessage: response?.message, alertType: response?.type }));
+        return response;
+    };
+
+    return apiCall;
+};
+
+export default useKanakkuApi;
