c9e6f3843b9b6638dd1605bb74d3aba6f8ea4d1b
Vaseekaran-S <vaseekaransaminathan@gmail.com>
2024-09-08 23:34:44 +0530

    ui: accounts card enhancement

--- src/components/cards/accounts/skeleton.jsx
+++ src/components/cards/accounts/skeleton.jsx
@@ -1,24 +1,23 @@
 import React from 'react'
 import Card from 'components/cards'
 
-function AccountCardSkeleton({ name, balance }) {
+function AccountCardSkeleton() {
     return (
         <Card shadow={false}>
-            <div className="animate-pulse flex space-x-4">
-                <div className="rounded-full bg-slate-700 h-10 w-10"></div>
-                <div className="flex-1 space-y-6 py-1">
-                    <div className="h-2 bg-slate-700 rounded"></div>
-                    <div className="space-y-3">
-                        <div className="grid grid-cols-3 gap-4">
-                            <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                            <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-                        </div>
-                        <div className="h-2 bg-slate-700 rounded"></div>
+            <div className="animate-pulse">
+                <div className='flex items-center gap-2 mb-3'>
+                    <div className="rounded-lg bg-slate-700 h-9 w-9"></div>
+                    <span className="w-[50%] py-2 bg-slate-700 rounded"></span>
+                </div>
+                <div className="flex justify-between">
+                    <p className='bg-slate-700 inline px-5 py-3 rounded font-medium'></p>
+                    <div className='flex items-center gap-1'>
+                        <span className='bg-slate-700 rounded p-2'></span>
+                        <span className='bg-slate-700 rounded p-2'></span>
                     </div>
                 </div>
+                <div className="h-2 bg-slate-700 rounded mt-2 lg:w-[70%]"></div>
             </div>
-            <h2 className='text-xl font-bold mb-2'>{name}</h2>
-            <p className='bg-green-800 inline px-2 py-1 rounded font-medium text-white'>Rs {balance}</p>
         </Card>
     )
 }
