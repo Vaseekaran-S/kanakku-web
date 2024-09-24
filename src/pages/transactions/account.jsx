b6ccf4db1645671c02c15569a1b733bb61baba1d
Vaseekaran-S <vaseekaransaminathan@gmail.com>
2024-09-24 21:26:30 +0530

    perf: improve performance by implement redux for accounts

--- src/pages/transactions/account.jsx
+++ src/pages/transactions/account.jsx
@@ -1,48 +1,47 @@
-import { getAccountByUrl } from 'api/accounts';
-import { getTransactionsByAccount } from 'api/transactions';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+import { selectAccountByUrl } from 'redux-store/accounts/accountSlice';
+
+
+import ToggleBtn from 'components/buttons/toggle';
 import Card from 'components/cards';
 import AccountHeader from 'components/header/account';
 import PageSection from 'components/sections/page'
 import TransactionsTable from 'components/tables/transactions';
-import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux';
-import { useNavigate, useParams } from 'react-router-dom'
 
 function AccountTransaction() {
     const navigate = useNavigate();
     const { url } = useParams();
-    const { _id: userId } = useSelector((store) => store.user.userData);
-
-    const [isLoading, setIsLoading] = useState(true);
-    const [account, setAccount] = useState({});
+    const account = useSelector(state => selectAccountByUrl(state.accounts, url));
+    const [type, setType] = useState("Both");
 
     useEffect(() => {
-        const fetchAccount = async () => {
-            try {
-                const account = await getAccountByUrl(userId, url);
-                if (account?.type === "error") return navigate("/accounts");
-                setAccount(account);
-            } finally {
-                setIsLoading(false);
-            }
-        };
-        fetchAccount();
+        if (!Object.keys(account).length) return navigate("/accounts");
     }, [url])
 
-    const { name, icon, balance } = account;
+    const { name, icon } = account;
 
     return (
         <PageSection>
             <AccountHeader url={url} name={name} icon={icon} />
             <div className="grid grid-cols-12 gap-5 pt-4">
-                <div className="col-span-3">
+                <div className="col-span-12 lg:col-span-3">
                     <Card shadow={false}>
+                        <h5 className='mb-3 font-bold text-xl'>Filters:</h5>
+                        <div className="flex gap-2 pb-5">
+                            {["Income", "Expense", "Both"].map((label) => (
+                                <ToggleBtn key={label} active={type === label} onClick={() => setType(label)} customCss="rounded-lg w-full">
+                                    {label}
+                                </ToggleBtn>
+                            ))}
+                        </div>
                     </Card>
                 </div>
-                <div className="col-span-9">
+                <div className="col-span-12 lg:col-span-9">
                     <Card shadow={false}>
                         <h2 className='font-bold text-xl mb-5'>Transactions History</h2>
-                        { account?._id && <TransactionsTable accountId={account?._id}/> }
+                        {account?._id && <TransactionsTable accountId={account?._id} />}
                     </Card>
                 </div>
             </div>
